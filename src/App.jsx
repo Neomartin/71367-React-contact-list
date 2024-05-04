7c8b5e88fe9f2dad10e76af7e2bb9b274d1eae25
Martin Rodriguez <neomartinr@gmail.com>
2024-05-03 22:19:52 -0300

    Añadir usuario y borrar funcionando

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
 import { useEffect, useState } from 'react';
 import axios from 'axios';
+import Swal from 'sweetalert2';
+import Icon from 'react-icons-kit';
+import { bin, pencil } from 'react-icons-kit/icomoon'
 
 import './App.css'
 
@@ -9,31 +12,29 @@ function App() {
   const [ users, setUsers ] = useState([]);
 
   useEffect(() => {
-
       getContactList();
-
-  }, [])
-    // Use effect nos permite ejecutar código después de que el componente se renderiza y de manera controlada
+  }, []);
+  
+  // Use effect nos permite ejecutar código después de que el componente se renderiza y de manera controlada
 
   async function getContactList() {
     // Obtener los usuarios desde mockapi y vamos a actualizar el estado users
     try {
-      console.log(`Antes del await`)
-      const usuarios = await axios.get(`${URL}/contact-list`).data;
-      setUsers(usuarios)
-      
-      const primerUser = usuarios[0];
-      const user = await axios.get(`${URL}/contact-list/${primerUser.id}`).data;
 
-      const update = await axios.put(`${URL}/contact-list/${user.id}`, user).data;
+      const { data } = await axios.get(`${URL}/contact-list`);
+
+      setUsers(data)
+
     } 
     catch(error) {
-      console.log(error)
+      console.log(error);
+      Swal.fire({
+        icon: 'error',
+        title: 'Oops...',
+        text: 'Hubo un error al obtener los contactos',
+      })
     } 
     
-
-
-
 
     // axios.get(`${URL}/contact-list`)
     //         .then(response => {
@@ -67,20 +68,162 @@ function App() {
     //         })
   }
 
-  // !getContactList();
+  function handleSubmit(evt) {
+    evt.preventDefault();
+
+    // const formDataObj = new FormData(evt.target)
+    // const newContact = Object.fromEntries(  formDataObj.entries()   );
+
+    // formatData(newContact)
+    // newContact.bornDate = new Date(newContact.bornDate).getTime()
+
+    const el = evt.target.elements;
+
+    const newContact = {
+      fullname: el.fullname.value,
+      bornDate: new Date(el.bornDate.value).getTime(),
+      email: el.email.value,
+      phone: el.phone.value,
+      image: el.image.value,
+    }
+
+    console.log(newContact)
+
+    createNewContact(newContact)
+
+  }
+
+  async function createNewContact(contacto) {
+    try {
+      const response = await axios.post(`${URL}/contact-list`, contacto);
+      setUsers( [ ...users, response.data ]  )
+      // getContactList();
+    }
+    catch(error) {
+      console.log(error)
+    }
+  }
+
+  // Borrar usuario
+  function handleDelete(id) {
+
+    try {
+
+      Swal.fire({
+        title: "Eliminar usuario",
+        text: "Realmente desea eleminar el usuario?",
+        icon: "warning",
+        showConfirmButton: true,
+        confirmButtonText: "Borrar!",
+        showCancelButton: true,
+
+      }).then(async(action) => {
+        if(action.isConfirmed) {
+          
+          await axios.delete(`${URL}/contact-list/${id}`);
+
+          getContactList()
+        }
+
+      })
+
+
+    }
+    catch(error) {
+      console.log(error)
+    }
+
+  }
+
+
+
+
+
+
+
+  function handleEdit(id) {}
+
+
 
   return (
     <>
-      <h1>Lista de Contactos</h1>
-      <ul>
-        {
-          users.map(usuario => {
-            return (
-              <li key={usuario.id}>{ usuario.fullname }</li>
-            )
-          })
-        }
-      </ul>
+      <div className="contact-list-container">
+
+        <div className="contact-form-wrapper">
+          <h1>Añadir Contacto</h1>
+          <form className="contact-form" onSubmit={handleSubmit}>
+
+            <div className="input-group">
+              <label htmlFor="fullname">Nombre Completo</label>
+              <input type="text" id="fullname" name="fullname" />
+            </div>
+            <div className="input-group">
+              <label htmlFor="bornDate">Fecha de Nacimiento</label>
+              <input type="date" id="bornDate" name="bornDate" />
+            </div>
+            <div className="input-group">
+              <label htmlFor="email">Correo Electrónico</label>
+              <input type="email" id="email" name="email" />
+            </div>
+            <div className="input-group">
+              <label htmlFor="phone">Teléfono</label>
+              <input type="text" id="phone" name="phone" />
+            </div>
+            <div className="input-group">
+              <label htmlFor="image">Enlace a la imagen</label>
+              <input type="url" id="image" name="image" />
+            </div>
+            <div className="input-group">
+              <button type="submit">Añadir Contacto</button>
+            </div>
+
+          </form>
+        </div>
+
+        <div className="contact-list">
+          <h1>Lista de Contactos</h1>
+          <ul>
+            {
+              users.map(usuario => {
+                return (
+                  <li key={usuario.id} className="contact">
+                    <div className="contact-image-wrapper">
+                      <img className="contact-image" src={usuario.image} alt="" />  
+                    </div>  
+                    <div className="contact-user">
+                      <div className="contact-fullname">
+                        { usuario.fullname }
+                      </div>
+                      <span className="contact-date">
+                        { usuario.bornDate }
+                      </span>
+                    </div>
+                    <div className="contact-email">
+                      { usuario.email }
+                    </div>
+                    <div className="contact-phone">
+                      { usuario.phone }
+                    </div>
+                    <div className="contact-actions">
+                      <button onClick={ () => handleEdit(usuario.id) } >
+                        <Icon icon={ pencil } />
+                      </button>
+
+                      <button className='danger' onClick={ () => handleDelete(usuario.id) }>
+                        <Icon icon={ bin } />
+                      </button>
+
+                    </div>
+                  </li>
+                )
+              })
+            }
+          </ul>
+        </div>
+        
+      </div>
+
+      
     </>
   )
 }
